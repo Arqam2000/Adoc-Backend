620d053c025e6006741b47334b9a12faf21dbe1c
zareensharaf <kaysoftware@hotmail.com>
2025-12-19 12:00:42 +0500

    add file path

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -3,7 +3,7 @@ import multer from 'multer'
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // Specify the directory where files should be saved
-    cb(null, 'uploads/'); 
+    cb(null, 'https://kaysoftwares.com/uploads'); 
   },
   filename: function (req, file, cb) {
     // Use the original name for the file
