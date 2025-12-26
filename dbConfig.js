b5f591e7c4824d4c8ff7d69792e3ba6b91672786
Arqam2000 <arqamsarwar9@gmail.com>
2025-12-26 16:19:06 +0500

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -9,8 +9,8 @@ import mysql from "mysql2/promise"
 export const pool = mysql.createPool({
   host: process.env.DB_HOST, // or your MySQL host
   user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  password: '',
+  password: process.env.DB_PASSWORD,
+  // password: '',
   database: process.env.DB_NAME
 });
 
