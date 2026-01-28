a1eb65df96825b0c0333d566c6828b60262ef2ce
Arqam2000 <arqamsarwar9@gmail.com>
2026-01-28 10:23:35 +0500

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -16,34 +16,37 @@ import mysql from "mysql2/promise"
 //   connectionLimit: 10,
 // });
 
-export const pool = mysql.createPool(process.env.DATABASE_URL);
+// export const pool = mysql.createPool(process.env.DATABASE_URL);
 
-// let pool;
+let pool;
 
-// async function initDB() {
-//   while (true) {
-//     try {
-//       pool = mysql.createPool({
-//         host: process.env.DB_HOST,
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASSWORD,
-//         database: process.env.DB_NAME,
-//         port: 3306,
-//         waitForConnections: true,
-//         connectionLimit: 10,
-//       });
+async function initDB() {
+  while (true) {
+    try {
+      pool = mysql.createPool(
+      //   {
+      //   host: process.env.DB_HOST,
+      //   user: process.env.DB_USER,
+      //   password: process.env.DB_PASSWORD,
+      //   database: process.env.DB_NAME,
+      //   port: 3306,
+      //   waitForConnections: true,
+      //   connectionLimit: 10,
+      // }
+      process.env.DATABASE_URL
+    );
 
-//       // 🔥 Force DNS + TCP check
-//       await pool.query("SELECT 1");
-//       console.log("✅ MySQL connected");
-//       break;
-//     } catch (err) {
-//       console.error("❌ MySQL not ready:", err.code);
-//       await new Promise(r => setTimeout(r, 5000));
-//     }
-//   }
-// }
+      // 🔥 Force DNS + TCP check
+      await pool.query("SELECT 1");
+      console.log("✅ MySQL connected");
+      break;
+    } catch (err) {
+      console.error("❌ MySQL not ready:", err.code);
+      await new Promise(r => setTimeout(r, 5000));
+    }
+  }
+}
 
-// await initDB();
+await initDB();
 
-// export { pool };
+export { pool };
